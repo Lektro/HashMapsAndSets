cbed7f09d7c8effb6856b0e8e2baf5060481b354
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-08-13 12:00:50 +0200

    added printstatements for the original sets to make it easier to read

--- src/main/java/be/vdab/Exercise21_1.java
+++ src/main/java/be/vdab/Exercise21_1.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public class Exercise21_1 {
     public static void main(String[] args) {
 
-        // String Array
+        // String Array holding the string value's
         String[] keywordString1 = {"Chemistry", "Mathematics", "Biology", "English"};
         String[] keywordString2 = {"Biology", "English", "Geography", "Physics"};
 
@@ -15,23 +15,27 @@ public class Exercise21_1 {
         Set<String> keywordSet1 = new HashSet<>(Arrays.asList(keywordString1));
         Set<String> keywordSet2 = new HashSet<>(Arrays.asList(keywordString2));
 
+        // Displays the original HashSets
+        System.out.println("Original Hashset1: " + keywordSet1);
+        System.out.println("Original Hashset2: " + keywordSet2);
+
         // Created new HashSets to deduct value's if equal
         Set one = new HashSet<>(keywordSet1);
         one.removeAll(keywordSet2);
         // Prints out the words only found in set 1
-        System.out.println(one + ": only present in HashSet 1.");
+        System.out.println(one + ": only present in Hashset 1.");
 
         // Created new HashSets to deduct value's if equal
         Set four = new HashSet<>(keywordSet2);
         four.removeAll(keywordSet1);
         // Prints out the words only found in set 2
-        System.out.println(four + ": only present in HashSet 2.");
+        System.out.println(four + ": only present in Hashset 2.");
 
         // check if an element is present in both strings
         for (final String element : keywordSet1) {
             // if second set has the current element
             if (keywordSet2.contains(element)) {
-                System.out.println(element + ": present in both HashSets.");
+                System.out.println(element + ": present in both Hashsets.");
             }
         }
     }

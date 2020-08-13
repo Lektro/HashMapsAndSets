0e893683b7d747552f0bf36f8418704ec6ecef85
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-08-13 14:34:48 +0200

    key set in fillhashmap done ready for retrieval

--- src/main/java/be/vdab/Exercise21_9.java
+++ src/main/java/be/vdab/Exercise21_9.java
@@ -11,23 +11,37 @@ public class Exercise21_9 {
         private static String fillHashMap(){
             Map<String, String> hashMap = new HashMap<>();
             hashMap.put("Alabama", "Montgomery");
+
+            // check if hashmap is filled or not
+            if(hashMap != null && !hashMap.isEmpty()){
+
+                // set keys in hashmap
+                for(String key : hashMap.keySet()){
+
+                    // print out each key set
+                    System.out.println(key+":"+hashMap.get(key));
+                }
+            }
+            
             return String.valueOf(hashMap);
         }
     }
 
+    // Ask Prompt to user and try to compare to hashmap
     public static class AskUserPrompt {
         private static String userInputState() {
             System.out.println("Enter a valid USA State: ");
-            Scanner sc = new Scanner(System.in);
-            String userInputState = sc.nextLine();
+            Scanner input = new Scanner(System.in);
+            String userInputState = input.nextLine();
+
             return userInputState;
         }
     }
 
     public static void main(String[] args) {
-        //System.out.println(MappingStatesToCapitols.fillHashMap());
-        AskUserPrompt.userInputState();
-        System.out.println();
+        MappingStatesToCapitols.fillHashMap();
+        //AskUserPrompt.userInputState();
+        //System.out.println();
     }
 
 }
